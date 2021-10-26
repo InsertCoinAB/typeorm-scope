c5aac873b9988c7f27300fd1e67706a14beb34cc
Christian Schwaderer <christian@insertcoin.se>
2021-10-26 12:00:05 +0200

    chore(deps): nodejs update to 16 (latest LTS)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	presets: [["@babel/preset-env", { targets: { node: "12" } }], "@babel/typescript"],
+	presets: [["@babel/preset-env", { targets: { node: "16" } }], "@babel/typescript"],
 	plugins: [
 		"babel-plugin-transform-typescript-metadata",
 		["@babel/plugin-proposal-decorators", { legacy: true }],
